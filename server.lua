2c2735c351149632dd2fd3d9b2b616f78554ef57
LuxuryScripts <110750740+LuxuryScripts@users.noreply.github.com>
2022-09-23 22:54:49 +0200

    Add files via upload

--- server.lua
+++ server.lua
@@ -4,23 +4,46 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 
 function RandomItem()
-   return Config.Items[math.random(#Config.Items)]
+  return Config.Items[math.random(#Config.Items)]
 end
 
-
-
+local used = false
+local counter = 1 
+local _source 
 
 ESX.RegisterUsableItem('box', function(source)
   local xPlayer = ESX.GetPlayerFromId(source)
 
-
+  used = true 
+  _source = source
   xPlayer.removeInventoryItem('box', 1)
-  
-  xPlayer.addInventoryItem(RandomItem(), 1)
-  xPlayer.addInventoryItem(RandomItem(), 1)
-  xPlayer.addInventoryItem(RandomItem(), 1)
-  xPlayer.addInventoryItem(RandomItem(), 1)
+
 end)
+
+
+
+
+Citizen.CreateThread(function()
+  while true do 
+    
+  if used == true then 
+  local xPlayer = ESX.GetPlayerFromId(_source)
+    print('test')
+
+    if counter <= Config.howmanyitems then 
+      counter = counter + 1
+      xPlayer.addInventoryItem(RandomItem(), 1)
+    else
+      used = false
+      counter = 1
+    end
+
+  end
+    Citizen.Wait(0)
+  end
+end)
+
+
 
 
 
